193fcd2c5db391ce28f9324a36851a67c416db43
Jefta <s1116917@student.windesheim.nl>
2017-11-22 13:14:36 +0100

    further style improvements

--- index.php
+++ index.php
@@ -1,37 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Toolwelle</title>
-        <link href="css/style.css" rel="stylesheet">
-        <link href="css/color.css" rel="stylesheet">
-        <link href="css/responsive.css" rel="stylesheet">
-        <link href="css/font-awesome.min.css" rel="stylesheet" >
-    </head>
-    <style media="screen">
-      .contactIcon i,
-      .contact h3:after{
-          background-color: #e95b12;
-      }
+	<head>
+		<title>Toolwelle</title>
+		<meta charset="utf-8" />
+		<link href="css/style.css" rel="stylesheet" />
+		<link href="css/color.css" rel="stylesheet" />
+		<link href="css/responsive.css" rel="stylesheet" />
+		<link href="css/font-awesome.min.css" rel="stylesheet" />
 
-      .sendButton{
-        background-color: #e95b12;
-      }
-    </style>
-    <body>
-        <?php include 'include/menu.php'; ?>
-        <?php include 'include/home.php'; ?>
-        <?php include 'include/about.php'; ?>
-        <?php include 'include/products.php'; ?>
-        <?php include 'include/footer.php'; ?>
-    </body>
+		<style media="screen">
+
+			.contactIcon i, .contact h3:after{
+				background-color: #e95b12;
+			}
+
+			.sendButton{
+				background-color: #e95b12;
+			}
+
+		</style>
+	</head>
+	<body>
+		<?php include 'include/menu.php'; ?>
+		<?php include 'include/home.php'; ?>
+		<?php include 'include/about.php'; ?>
+		<?php include 'include/products.php'; ?>
+		<?php include 'include/footer.php'; ?>
+
+		<!-- Libraries -->
+		<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+		<script src="js/jquery.visible.min.js" type="text/javascript"></script>
+		<script src="js/modernizr.js" type="text/javascript"></script>
+		<script src="js/parallax.min.js" type="text/javascript"></script>
+		<script src="js/scroll_anchor.js" type="text/javascript"></script>
+		<script src="js/smoothscroll.js" type="text/javascript"></script>
+
+		<!-- Own scripts-->
+		<script src="js/scripts.js" type="text/javascript"></script>
+	</body>
 </html>
-<!-- libery's -->
-<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
-<script src="js/jquery.visible.min.js" type="text/javascript"></script>
-<script src="js/modernizr.js" type="text/javascript"></script>
-<script src="js/parallax.min.js" type="text/javascript"></script>
-<script src="js/scroll_anchor.js" type="text/javascript"></script>
-<script src="js/smoothscroll.js" type="text/javascript"></script>
-<!--Eigen scripts-->
-<script src="js/scripts.js" type="text/javascript"></script>
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $('#aboutContainer').parallax({
-    imageSrc: 'img/bg/tools.jpg'
+	imageSrc: 'img/bg/tools.jpg'
 });
 
 // Scrolling
@@ -15,126 +15,126 @@ var shrinkOn;
 
 // Set scroll for desktop nav
 function nav_desktop_check() {
-    distanceY = window.pageYOffset || document.documentElement.scrollTop;
-    shrinkOn = 100;
+	distanceY = window.pageYOffset || document.documentElement.scrollTop;
+	shrinkOn = 100;
 
-    // Run the header script
-    if(distanceY > shrinkOn) {
-        if(nav_state === 0) {
-            nav_hamburger();
-        }
-    }
-    else {
-        if(nav_state === 1) {
-            if($(window).width() >= 900) {
-                nav_normal_desktop();
-            }
-        }
-    }
+	// Run the header script
+	if(distanceY > shrinkOn) {
+		if(nav_state === 0) {
+			nav_hamburger();
+		}
+	}
+	else {
+		if(nav_state === 1) {
+			if($(window).width() >= 900) {
+				nav_normal_desktop();
+			}
+		}
+	}
 }
 
 // Tablet nav check
 function tablet_nav_check() {
-    if(nav_state === 0) {
-        if($(window).width() <= 900) {
-            nav_hamburger();
-        }
-    }
+	if(nav_state === 0) {
+		if($(window).width() <= 900) {
+			nav_hamburger();
+		}
+	}
 }
 tablet_nav_check();
 
 
 // Hambutton onclick
 hamburger.click(function() {
-    if(nav_state === 1) {
-        if($(window).width() >= 900) {
-            nav_normal_desktop();
-        }
-        else {
-            nav_normal_mobile();
-        }
-        logo_animation();
-    }else {
-        nav_hamburger()
-    }
+	if(nav_state === 1) {
+		if($(window).width() >= 900) {
+			nav_normal_desktop();
+		}
+		else {
+			nav_normal_mobile();
+		}
+		logo_animation();
+	}else {
+		nav_hamburger()
+	}
 });
 
 // Nav to hamburger
 function nav_hamburger() {
-    hamburger.removeClass("active")
-        navOverflow.animate({
-        width: 0
-    }, nav_ani_speed, function(){
-        hamburger.addClass("active")
-    });
-    nav_state = 1;
-    logo_animation();
+	hamburger.removeClass("active")
+		navOverflow.animate({
+		width: 0
+	}, nav_ani_speed, function(){
+		hamburger.addClass("active")
+	});
+	nav_state = 1;
+	logo_animation();
 }
 
 // Hamburger to nav
 function nav_normal_desktop() {
-    hamburger.addClass("active");
-    hamburger.removeClass("active");
-    navOverflow.css("width", "auto");
-    nav_witdh = navOverflow.innerWidth();
-    navOverflow.css("width", 0);
-    navOverflow.animate({
-        width: nav_witdh
-    }, nav_ani_speed, function(){
-        hamburger.removeClass("active")
-    });
-    nav_state = 0;
+	hamburger.addClass("active");
+	hamburger.removeClass("active");
+	navOverflow.css("width", "auto");
+	nav_witdh = navOverflow.innerWidth();
+	navOverflow.css("width", 0);
+	navOverflow.animate({
+		width: nav_witdh
+	}, nav_ani_speed, function() {
+		hamburger.removeClass("active")
+	});
+	nav_state = 0;
 
 }
 
 function nav_normal_mobile() {
-    navOverflow.animate({
-        width: "100%"
-    }, nav_ani_speed, function(){
-        hamburger.removeClass("active")
-    });
-    nav_state = 0;
+	navOverflow.animate({
+		width: "100%"
+	}, nav_ani_speed, function() {
+		hamburger.removeClass("active")
+	});
+	nav_state = 0;
 }
 
 // Logo aperances
 function logo_animation() {
-    if(page_end_logo_nav === false) {
-        if($(window).width() >= 1500) {
-            logoContainer.addClass("active")
-        }
-        else {
-            if(nav_state === 0) {
-                logoContainer.removeClass("active")
-            }
-            else {
-                logoContainer.addClass("active")
-            }
+	if(page_end_logo_nav === false) {
+		if($(window).width() >= 1500) {
+			logoContainer.addClass("active")
+		}
+		else {
+			if(nav_state === 0) {
+				logoContainer.removeClass("active")
+			}
+			else {
+				logoContainer.addClass("active")
+			}
 
-        }
-    }
-    else {
-        if($(window).width() >= 1500) {
-            logoContainer.removeClass("active")
-        }
-        else {
-            if(nav_state === 1) {
-                logoContainer.removeClass("active")
-            }
-        }
-    }
+		}
+	}
+	else {
+		if($(window).width() >= 1500) {
+			logoContainer.removeClass("active")
+		}
+		else {
+			if(nav_state === 1) {
+				logoContainer.removeClass("active")
+			}
+		}
+	}
 }
 logo_animation();
 
 // Event listeners
 $(window).scroll(function() {
-    page_end_logo_nav = $("#page_end_logo_nav").visible();
-    // Logo animation
-    logo_animation();
-    // Check if desktop is activated
-    nav_desktop_check()
+	page_end_logo_nav = $("#page_end_logo_nav").visible();
+	// Logo animation
+	logo_animation();
+	// Check if desktop is activated
+	nav_desktop_check()
 });
 
 $(window).resize(function() {
 	// If smaller then 900px remove nav
-    tablet_nav_check()
+	tablet_nav_check()
 });
