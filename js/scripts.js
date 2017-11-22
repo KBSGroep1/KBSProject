b862f3d6288f7b42a0f2dd27e908327e6175714b
Jefta <s1116917@student.windesheim.nl>
2017-11-22 13:02:03 +0100

    made js follow guidelines better

--- js/scripts.js
+++ js/scripts.js
@@ -2,76 +2,77 @@ $('#aboutContainer').parallax({
     imageSrc: 'img/bg/tools.jpg'
 });
 
-//scrolling
+// Scrolling
 var nav = $("#navId");
 var navOverflow = $("#navOverflow");
 var page_end_logo_nav = $("#page_end_logo_nav").visible();
 var logoContainer = $("#logoContainer");
-var nav_ani_speed = 200 //in ms
-var nav_state = 0 // 0 is nav  1 is hamburger visable
-var hamburger = $("#hamburgermenu") //hamburger elemnt
+var nav_ani_speed = 200; // In ms
+var nav_state = 0; // 0 is nav, 1 is hamburger visable
+var hamburger = $("#hamburgermenu");
 var distanceY;
 var shrinkOn;
 
-//set scroll for desktop nav
-function nav_desktop_check(){
+// Set scroll for desktop nav
+function nav_desktop_check() {
     distanceY = window.pageYOffset || document.documentElement.scrollTop;
     shrinkOn = 100;
 
-    //run the header script
-    if (distanceY > shrinkOn){
-        if(nav_state === 0){
+    // Run the header script
+    if(distanceY > shrinkOn) {
+        if(nav_state === 0) {
             nav_hamburger();
         }
-    } else{
-        if (nav_state === 1 ){
-            if ($(window).width() >= 900){
+    }
+    else {
+        if(nav_state === 1) {
+            if($(window).width() >= 900) {
                 nav_normal_desktop();
             }
         }
     }
 }
 
-//tablet nav check
-function tablet_nav_check(){
-    if (nav_state === 0){
-        if ($(window).width() <= 900){
-        nav_hamburger();
+// Tablet nav check
+function tablet_nav_check() {
+    if(nav_state === 0) {
+        if($(window).width() <= 900) {
+            nav_hamburger();
         }
     }
 }
-tablet_nav_check()
+tablet_nav_check();
 
 
-//hambutton onclikc
-hamburger.click(function(){
-    if (nav_state === 1){
-        if ($(window).width() >= 900){
-        nav_normal_desktop();
-        } else{
+// Hambutton onclick
+hamburger.click(function() {
+    if(nav_state === 1) {
+        if($(window).width() >= 900) {
+            nav_normal_desktop();
+        }
+        else {
             nav_normal_mobile();
         }
-
         logo_animation();
-    } else{
+    }else {
         nav_hamburger()
     }
 });
 
-//nav to hamburger
-function nav_hamburger(){
+// Nav to hamburger
+function nav_hamburger() {
     hamburger.removeClass("active")
         navOverflow.animate({
         width: 0
-      }, nav_ani_speed, function(){
-            hamburger.addClass("active")
-      });
+    }, nav_ani_speed, function(){
+        hamburger.addClass("active")
+    });
     nav_state = 1;
     logo_animation();
 }
 
-//hamburger to nav
-function nav_normal_desktop(){
+// Hamburger to nav
+function nav_normal_desktop() {
     hamburger.addClass("active");
     hamburger.removeClass("active");
     navOverflow.css("width", "auto");
@@ -85,7 +86,8 @@ function nav_normal_desktop(){
     nav_state = 0;
 
 }
-function nav_normal_mobile(){
+
+function nav_normal_mobile() {
     navOverflow.animate({
         width: "100%"
     }, nav_ani_speed, function(){
@@ -94,48 +96,45 @@ function nav_normal_mobile(){
     nav_state = 0;
 }
 
-//logo apirances
-function logo_animation(){
-    if (page_end_logo_nav === false){
-        if ($(window).width() >= 1500){
-                logoContainer.addClass("active")
-        } else{
-            if (nav_state === 0){
+// Logo aperances
+function logo_animation() {
+    if(page_end_logo_nav === false) {
+        if($(window).width() >= 1500) {
+            logoContainer.addClass("active")
+        }
+        else {
+            if(nav_state === 0) {
                 logoContainer.removeClass("active")
-            } else{
+            }
+            else {
                 logoContainer.addClass("active")
             }
 
         }
-
-    } else{
-        if ($(window).width() >= 1500){
+    }
+    else {
+        if($(window).width() >= 1500) {
             logoContainer.removeClass("active")
-        } else{
-            if (nav_state === 1){
+        }
+        else {
+            if(nav_state === 1) {
                 logoContainer.removeClass("active")
             }
-
         }
     }
 }
 logo_animation();
 
-
-
-
-
-//eventlisnteres
-$(window).scroll(function(){
+// Event listeners
+$(window).scroll(function() {
     page_end_logo_nav = $("#page_end_logo_nav").visible();
-    //logo animation
+    // Logo animation
     logo_animation();
-    //check if desktop is activated
+    // Check if desktop is activated
     nav_desktop_check()
 });
 
-$(window).resize(function(){
-	//if smaller then 900 remove nav
+$(window).resize(function() {
+	// If smaller then 900px remove nav
     tablet_nav_check()
-
 });
