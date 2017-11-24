79f55143e3aff953728b0b990bb32b3b90a2ae51
barryvdvegt <32839265+barryvdvegt@users.noreply.github.com>
2017-11-24 12:35:54 +0100

    updated cms frontpage style

--- cms.php
+++ cms.php
@@ -19,37 +19,65 @@ try {
 		echo "Failed to connect to database";
 		exit;
 	}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>Wilpe CMS</title>
 		<meta charset="UTF-8" />
-		<link rel="stylesheet" type="text/css" href="css/cms.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/cms.css">	
 	</head>
 	<body>
-		<div id="topBar">
-			<img src="img/cms/wilpeLogo.jpg">
-			<p id="username">Ingelogd als: <a href="users.php"><?php print($_SESSION["username"]) ?></a></p>
-		</div>
+		<nav class="navbar navbar-inverse">
+  			<div class="container-fluid">
+    			<div class="navbar-header">
+    		  		<a class="navbar-brand" href="#"><img id="logoWilpe"src="img/cms/wilpeLogo.jpg"></a>
+   				</div>
+    		  		<p id="username">Ingelogd als: <a href="users.php"><?php print($_SESSION["username"]) ?></a><br>
+    		  			<a href="logout.php">Uitloggen</a></p>
+  			</div>
+		</nav>
+  <div id="sidebar">
+            <nav id="spy">
+                <ul class="sidebar-nav nav">
+                    <li class="sidebar-brand">
+                    	<select id="websiteSelect">
+							<option value="">Selecteer een site</option>
+							<?php
+								$stmt = $dbh->prepare("SELECT websiteID, name FROM website");
+								$stmt->execute();
+								while ($result = $stmt->fetch()) {
+									print("<option value='" . $result["websiteID"] . "'>" . $result["name"] . "</option>");
+								}
+							?>
+						</select>
+                    </li>
+                    <li>
+                        <a href="producten.php" data-scroll>
+                            <span class="fa fa-anchor solo">Producten</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="website.php" data-scroll>
+                            <span class="fa fa-anchor solo">Website</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="users.php" data-scroll>
+                            <span class="fa fa-anchor solo">Gebruikers</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="messages.php" data-scroll>
+                            <span class="fa fa-anchor solo">Berichten</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
 		<br>
-		<div id="sideBar">
-			<select>
-				<option value="">Selecteer een site</option>
-				<?php
-					$stmt = $dbh->prepare("SELECT websiteID, name FROM website");
-					$stmt->execute();
-					while ($result = $stmt->fetch()) {
-						print("<option value='" . $result["websiteID"] . "'>" . $result["name"] . "</option>");
-					}
-				?>
-			</select><br>
-			<a href="producten.php">Producten</a><br>
-			<a href="thema.php">Thema aanpassen</a><br>
-			<a href="users.php">Gebruikers</a><br>
-			<a href="websites.php">Websites</a><br>
-		</div>
 	</body>
 </html>
 <?php
