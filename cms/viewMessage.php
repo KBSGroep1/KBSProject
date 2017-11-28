fb4a365c3fb43568257530689fcbf05474cdd765
barryvdvegt <32839265+barryvdvegt@users.noreply.github.com>
2017-11-29 00:13:23 +0100

    Added mail to messages

--- cms/viewMessage.php
+++ cms/viewMessage.php
@@ -15,11 +15,11 @@ include 'include/topBar.php';
 </nav>
 <div class="message">
  <?php
-  $stmt = $dbh->prepare("SELECT * FROM contact C WHERE C.contactID = :id ");
+  $stmt = $dbh->prepare("SELECT customerName, timestamp, email, C.websiteID, name, text FROM contact C JOIN website W on W.websiteID = C.websiteID WHERE C.contactID = :id ");
   $stmt->bindParam(":id", $_GET["message"]);
   $stmt->execute();
   while ($result = $stmt->fetch()) {
-      print("<h3>" . $result["customerName"] . "\n" . $result["timestamp"] . "</h3> <p> <a>". $result["email"] . "</a><br>" . $result["text"] . "\n" .  "</p>");
+      print("<h3>" . $result["customerName"] . "\n" . $result["timestamp"] . "</h3> <p> <a href='mailto:" . $result['email'] . "?Subject=". $result['name'] ."'>". $result["email"] . "</a><br>" . $result["text"] . "\n" .  "</p>");
   }
  ?>
 </div>
