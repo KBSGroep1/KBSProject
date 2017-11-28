16e00eb92c40dd9be4e62d18cb651f7bae7f52a9
Jefta <s1116917@student.windesheim.nl>
2017-11-28 15:44:01 +0100

    first part of contact form

--- include/footer.php
+++ include/footer.php
@@ -1,65 +1,74 @@
 <div class="detailsBackground" id="pageContact">
-    <div class="contactDetail paddingTop100 paddingBottom70">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="contact">
-                        <h3 class="marginBottom50"><?php echo $texts["footerLeftTitle"]; ?></h3>
-                        <p><?php echo $texts["footerLeftText"]; ?></p>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="contact">
-                        <h3 class="marginBottom50"><?php echo $texts["contactTitle"]; ?></h3>
-                        <input type="text" name="full name" placeholder="Name">
-                        <input type="text" name="email address" placeholder="Email">
-                        <textarea placeholder="Message" rows="6"></textarea>
-                        <button type="submit" class="sendButton buttonColor marginBottom30"><?php echo $texts["contactSendButton"]; ?></button>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12 col-xs-12">
-                    <div class="contact">
-                        <h3 class="marginBottom50"><?php echo $texts["footerRightTitle"]; ?></h3>
-                        <p><?php echo $texts["footerRightTitle"]; ?></p>
-                    </div>
-                    <div class="col-md-2 col-sm-2 col-xs-2">
-                        <div class="row">
-                            <div class="contactIcon">
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10 col-xs-10">
-                        <div class="contactIcon marginBottom20">
-                            <p><?php echo $texts["contactLocation"]; ?></p>
-                        </div>
-                    </div>
-                    <div class="col-md-2 col-sm-2 col-xs-2">
-                        <div class="row">
-                            <div class="contactIcon">
-                                <i class="fa fa-phone" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10 col-xs-10">
-                        <div class="contactIcon marginBottom20">
-                            <p><?php echo $texts["contactPhone"]; ?></p>
-                        </div>
-                    </div>
-                    <div class="col-md-2 col-sm-2 col-xs-2">
-                        <div class="row">
-                            <div class="contactIcon">
-                                <i class="fa fa-envelope" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10 col-xs-10">
-                        <div class="contactIcon marginBottom20">
-                            <p><?php echo $texts["contactEmail"]; ?></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="contactDetail paddingTop100 paddingBottom70">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="contact">
+						<h3 class="marginBottom50"><?php echo $texts["footerLeftTitle"]; ?></h3>
+						<p><?php echo $texts["footerLeftText"]; ?></p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<form method="POST" action="submitContact.php">
+						<div class="contact">
+							<h3 class="marginBottom50"><?php echo $texts["contactTitle"]; ?></h3>
+<?php
+if(isset($_SESSION["contactError"]) && !empty($_SESSION["contactError"])) {
+	// TODO: this currenlty uses inline styling, please fix
+	echo "<div id='contactError' style='color: white; padding: 6px; font-weight: bold; margin-bottom: 8px; background-color: red'>" . $_SESSION["contactError"] . "</div>";
+	$_SESSION["contactError"] = null;
+}
+?>
+							<input type="text" name="fullname" required placeholder="Name">
+							<input type="email" name="email" placeholder="Email">
+							<textarea placeholder="Message" name="message" required rows="6"></textarea>
+							<button type="submit" class="sendButton buttonColor marginBottom30"><?php echo $texts["contactSendButton"]; ?></button>
+						</div>
+					</form>
+				</div>
+				<div class="col-md-4 col-sm-12 col-xs-12">
+					<div class="contact">
+						<h3 class="marginBottom50"><?php echo $texts["footerRightTitle"]; ?></h3>
+						<p><?php echo $texts["footerRightTitle"]; ?></p>
+					</div>
+					<div class="col-md-2 col-sm-2 col-xs-2">
+						<div class="row">
+							<div class="contactIcon">
+								<i class="fa fa-map-marker" aria-hidden="true"></i>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-10 col-sm-10 col-xs-10">
+						<div class="contactIcon marginBottom20">
+							<p><?php echo $texts["contactLocation"]; ?></p>
+						</div>
+					</div>
+					<div class="col-md-2 col-sm-2 col-xs-2">
+						<div class="row">
+							<div class="contactIcon">
+								<i class="fa fa-phone" aria-hidden="true"></i>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-10 col-sm-10 col-xs-10">
+						<div class="contactIcon marginBottom20">
+							<p><?php echo $texts["contactPhone"]; ?></p>
+						</div>
+					</div>
+					<div class="col-md-2 col-sm-2 col-xs-2">
+						<div class="row">
+							<div class="contactIcon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-10 col-sm-10 col-xs-10">
+						<div class="contactIcon marginBottom20">
+							<p><?php echo $texts["contactEmail"]; ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
---
+++ thankyou.php
@@ -0,0 +1,102 @@
+<?php
+// TODO: different colors and fonts for each website
+
+session_start();
+
+if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true) {
+	$_SESSION["loggedIn"] = false;
+	$_SESSION["userID"] = -1;
+	$_SESSION["username"] = null;
+	$_SESSION["role"] = 0;
+}
+
+$config = parse_ini_file("config/config.ini");
+
+$requestedWebsite = "toolwelle.com"; // TODO: fix
+
+try {
+	$dbh = new PDO("mysql:"
+		. "host=" . $config["host"]
+		. ";port=" . $config["port"]
+		. ";dbname=" . $config["db"],
+		$config["username"], $config["password"]);
+}
+catch(PDOException $e) {
+	echo "Failed to connect to database";
+	exit;
+}
+
+$stmt = $dbh->prepare("SELECT * FROM website WHERE name = :name");
+$stmt->bindParam(":name", $requestedWebsite);
+$stmt->execute();
+
+if($stmt->rowCount() !== 1) {
+	http_response_code(404);
+	echo "Page not found";
+	exit;
+}
+
+$result = $stmt->fetch();
+
+$stmt2 = $dbh->prepare("SELECT * FROM text WHERE websiteID = :id");
+$stmt2->bindParam(":id", $result["websiteID"]);
+$stmt2->execute();
+
+$texts = [];
+
+while($t = $stmt2->fetch()) {
+	$texts[$t["textName"]] = $t["text"];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Toolwelle</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/style.css" rel="stylesheet" />
+		<link href="css/font-awesome.min.css" rel="stylesheet" />
+
+		<style media="screen">
+
+			*, *:before, *:after {
+				box-sizing: border-box;
+				padding: 0;
+				margin: 0;
+			}
+
+			body {
+				background: url(img/<?php echo $result["bg1Path"]; ?>) no-repeat center center fixed;
+				background-size: cover;
+			}
+
+			#thankyou {
+				margin: auto;
+				margin-top: 260px;
+				width: 100vw;
+				max-width: 600px;
+				color: white;
+				padding: 64px;
+				background-color: rgba(56, 56, 56, .95) /* todo: get from DB */;
+			}
+
+			#thankyou h1 {
+				color: white;
+			}
+
+			#thankyou a {
+				color: white;
+				text-decoration: underline;
+				font-weight: bold;
+			}
+
+		</style>
+	</head>
+	<body>
+		<div id="thankyou">
+			<h1><?php echo $texts["contactThankyouTitle"]; ?></h1>
+			<p><?php echo $texts["contactThankyouText"]; ?></p>
+			<a href="/"><?php echo $texts["contactThankyouLink"]; ?></a>
+		</div>
+	</body>
+</html>
