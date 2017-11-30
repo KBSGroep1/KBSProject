fd1c3e75039faee7ce9221881b95196a02238ddf
marijnkoel <marijnkoel@live.nl>
2017-11-30 15:23:18 +0100

    paralax fix

--- include/products.php
+++ include/products.php
@@ -16,6 +16,39 @@
           </div>
         </figcaption>
       </figure>
+      <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product1"/>
+        <figcaption>
+          <div class="productText">
+            <h3>Toolwelle trolley</h3>
+            <p>Black edition</p>
+          </div>
+          <div class="price">
+            €19.00
+          </div>
+        </figcaption>
+      </figure>
+      <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product1"/>
+        <figcaption>
+          <div class="productText">
+            <h3>Toolwelle trolley</h3>
+            <p>Black edition</p>
+          </div>
+          <div class="price">
+            €19.00
+          </div>
+        </figcaption>
+      </figure>
+      <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product1"/>
+        <figcaption>
+          <div class="productText">
+            <h3>Toolwelle trolley</h3>
+            <p>Black edition</p>
+          </div>
+          <div class="price">
+            €19.00
+          </div>
+        </figcaption>
+      </figure>
       <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product2"/>
         <figcaption>
           <div class="productText">
@@ -38,5 +71,7 @@
           </div>
         </figcaption>
       </figure>
+      <div class="invisible">...</div>
+
   </div>
 </div>
