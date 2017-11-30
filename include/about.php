fdcfa9e1a8022bf566ff8c847076200ecfa5cd09
marijnkoel <marijnkoel@live.nl>
2017-11-30 12:07:34 +0100

    products centered + getting text from DB

--- include/about.php
+++ include/about.php
@@ -1,10 +1,10 @@
 <div class="about" id="pageAbout">
-  <div id="aboutContainer" class="paraContainer"></div>
+  <div id="aboutContainer" class="paraContainer">  <div class="blackLayer"></div></div>
   <div class="textContainer">
       <h1 class="titleAbout"><?php echo $texts["aboutTitle"]; ?></h1>
       <p class="bodyText">
         <?php echo $texts["aboutText"]; ?>
       </p>
   </div>
-  <div class="blackLayer"></div>
+
 </div>
--- include/products.php
+++ include/products.php
@@ -1,38 +1,42 @@
 <div class="products" id="pageProducts">
   <div id="productsContainer" class="paraContainer"><div class="blackLayer"></div></div>
-  <div class="containerProducts">
-    <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product1"/>
-      <figcaption>
-        <div class="productText">
-          <h3>Toolwelle trolley</h3>
-          <p>Black edition</p>
-        </div>
-        <div class="price">
-          €19.00
-        </div>
-      </figcaption>
-    </figure>
-    <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product2"/>
-      <figcaption>
-        <div class="productText">
-          <h3>Toolwelle trolley</h3>
-          <p>Black edition</p>
-        </div>
-        <div class="price">
-          €19.00
-        </div>
-      </figcaption>
-    </figure>
-    <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product3"/>
-      <figcaption>
-        <div class="productText">
-          <h3>Toolwelle trolley</h3>
-          <p>Black edition</p>
-        </div>
-        <div class="price">
-          €19.00
-        </div>
-      </figcaption>
-    </figure>
-</div>
+  <div class="textContainer">
+      <h1 class="titleAbout"><?php echo $texts["productTitle"]; ?></h1>
+      <p class="bodyText">
+        <?php echo $texts["productText"]; ?>
+      </p>
+      <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product1"/>
+        <figcaption>
+          <div class="productText">
+            <h3>Toolwelle trolley</h3>
+            <p>Black edition</p>
+          </div>
+          <div class="price">
+            €19.00
+          </div>
+        </figcaption>
+      </figure>
+      <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product2"/>
+        <figcaption>
+          <div class="productText">
+            <h3>Toolwelle trolley</h3>
+            <p>Black edition</p>
+          </div>
+          <div class="price">
+            €19.00
+          </div>
+        </figcaption>
+      </figure>
+      <figure class="productCard"><img src="img/testafbeeldingen/1.jpg" alt="product3"/>
+        <figcaption>
+          <div class="productText">
+            <h3>Toolwelle trolley</h3>
+            <p>Black edition</p>
+          </div>
+          <div class="price">
+            €19.00
+          </div>
+        </figcaption>
+      </figure>
+  </div>
 </div>
