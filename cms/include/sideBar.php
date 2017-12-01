e202d823eb1f6f6aec37641ed74a4a4fde93ffdc
barryvdvegt <32839265+barryvdvegt@users.noreply.github.com>
2017-12-01 18:44:53 +0100

    Added website view

--- cms/include/sideBar.php
+++ cms/include/sideBar.php
@@ -1,15 +1,18 @@
 <nav class="navbar navbar-default sidebar" role="navigation">
 	<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
 		<ul class="nav navbar-nav">
-		<li <?php if (fnmatch("*roduct*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="products.php">Producten</a></li>        
-		<li <?php if (fnmatch("*ite*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="sites.php">Website</a></li>        
-		<li <?php 
-			if ($_SESSION["userRole"] == 3) {
-				if (fnmatch("*ser*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="users.php">Gebruikers</a></li>
-			<?php 
-			} 
-			?>
-		<li <?php if (fnmatch("*essage*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="messages.php">Berichten</a></li>
+		<li <?php if (fnmatch("*roduct*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="products.php">Producten</a></li>   
+		<li <?php if (fnmatch("*ite*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="sites.php">Website</a></li>		     
+		<li <?php if (fnmatch("*ser*", $_SERVER["PHP_SELF"])) { print("class='active'");} if ($_SESSION["userRole"] < 3){ print("class='disabled'");}?>> <a <?php if($_SESSION["userRole"] < 3){
+		}else{
+			print("href='users.php'");
+		}
+		 ?>>Gebruikers</a></li>
+		<li <?php if (fnmatch("*essage*", $_SERVER["PHP_SELF"])) { print("class='active'");} if ($_SESSION["userRole"] < 2){ print("class='disabled'");}?>> <a <?php if($_SESSION["userRole"] < 2){
+		}else{
+			print("href='messages.php'");
+		}
+		 ?>>Berichten</a></li> 
 		</ul>
 	</div>
 	</div>
--- cms/viewMessage.php
+++ cms/viewMessage.php
@@ -5,12 +5,14 @@ include 'include/sideBar.php';
 ?>
 <div class="message">
  <?php
+  if ($_SESSION["userRole"] < 2) {
+ }else{
 	$stmt = $dbh->prepare("SELECT customerName, timestamp, email, C.websiteID, name, text FROM contact C JOIN website W on W.websiteID = C.websiteID WHERE C.contactID = :id ");
 	$stmt->bindParam(":id", $_GET["message"]);
 	$stmt->execute();
 	while ($result = $stmt->fetch()) {
 			print("<h3>" . $result["customerName"] . "\n" . $result["timestamp"] . "</h3> <p> <a href='mailto:" . $result['email'] . "?Subject=". $result['name'] ."'>". $result["email"] . "</a><br>" . $result["text"] . "\n" .  "</p>");
-	}
+	}}
  ?>
 </div>
 	</body>
