ce1d88fd0a9d7df07e6ca1640e3ccb4f9539489d
koendiender <koendiender@gmail.com>
2017-12-01 14:57:26 +0100

    added semicolon. whoops

--- cms/logout.php
+++ cms/logout.php
@@ -5,6 +5,6 @@ $_SESSION["userID"] = -1;
 $_SESSION["username"] = null;
 $_SESSION["role"] = 0;
 $_SESSION["loggedIn"] = false;
-$_SESSION["site"] = null
+$_SESSION["site"] = null;
 
 header("Location: /");
