44a1151263178b55a19a79b243265a54108fb0e2
koendiender <koendiender@gmail.com>
2017-12-01 13:12:37 +0100

    Fixed sidebar current page

--- cms/include/sideBar.php
+++ cms/include/sideBar.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-default sidebar" role="navigation">
 	<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
 		<ul class="nav navbar-nav">
-		<li ><a href="products.php">Producten</a></li>        
-		<li ><a href="sites.php">Website</a></li>        
-		<li ><a href="users.php">Gebruikers</a></li>
-		<li ><a href="messages.php">Berichten</a></li>
+		<li <?php if (fnmatch("*roduct*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="products.php">Producten</a></li>        
+		<li <?php if (fnmatch("*ite*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="sites.php">Website</a></li>        
+		<li <?php if (fnmatch("*ser*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="users.php">Gebruikers</a></li>
+		<li <?php if (fnmatch("*essage*", $_SERVER["PHP_SELF"])) { print("class='active'"); } ?>><a href="messages.php">Berichten</a></li>
 		</ul>
 	</div>
 	</div>
