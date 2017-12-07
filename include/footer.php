d9a2e9178363e1a1b85d036d1ae8e5c66304ec1a
marijnkoel <marijnkoel@live.nl>
2017-12-07 14:57:00 +0100

    detailfixes

--- include/footer.php
+++ include/footer.php
@@ -34,23 +34,11 @@ if(isset($_SESSION["contactError"]) && !empty($_SESSION["contactError"])) {
 					<div class="col-md-2 col-sm-2 col-xs-2">
 						<div class="row">
 							<div class="contactIcon">
-								<i class="fa fa-map-marker" aria-hidden="true"></i>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-10 col-sm-10 col-xs-10">
-						<div class="contactIcon marginBottom20">
-							<a class="textCollorEmail" href="https://www.google.nl/maps/place/Wilpe.com/@52.5615758,6.6335481,18.3z/data=!4m13!1m7!3m6!1s0x47c801d363d5e99f:0x5457d1bcfc1f438a!2sHandelsstraat+52,+7772+TR+Hardenberg!3b1!8m2!3d52.5616499!4d6.63382!3m4!1s0x47c8002cf076d953:0xa6e2b551ce17671c!8m2!3d52.5616499!4d6.63382" target="_blank" ><?php echo $texts["contactLocation"]; ?></a>
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-2 col-xs-2">
-						<div class="row">
-							<div class="contactIcon">
 								<i class="fa fa-phone" aria-hidden="true"></i>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-10 col-sm-10 col-xs-10">
+					<div class="col-md-10 col-sm-10 col-xs-10 marginTop12px">
 						<div class="contactIcon marginBottom20">
 							<a class="textCollorEmail" href="tel:+0637119910"><?php echo $texts["contactPhone"]; ?></a>
 						</div>
@@ -62,7 +50,7 @@ if(isset($_SESSION["contactError"]) && !empty($_SESSION["contactError"])) {
 							</div>
 						</div>
 					</div>
-					<div class="col-md-10 col-sm-10 col-xs-10">
+					<div class="col-md-10 col-sm-10 col-xs-10 marginTop12px">
 						<div class="contactIcon marginBottom20">
 							<a class="textCollorEmail" href="mailto:<?php print($texts['contactEmail']);?>" ><?php echo $texts["contactEmail"]; ?></a>
 						</div>
