862c496ae532b31735e79462a0aa48e88bb0113d
jamesdiender <koendiender@gmail.com>
2017-12-13 12:10:11 +0100

    Products come from the database now

--- include/products.php
+++ include/products.php
@@ -7,107 +7,26 @@
       </p>
   </div>
   <div class="textContainer2">
-      <figure class="productCard">
-        <div class="productInfoLayer visiblepanel" id="product1">
-          <h2 class="productName">Toolwelle trolley</h2>
-          This is the Black Edition of the tool carts from Toolwelle. It has 2 inlays fully filled with tools. It's possible to remove the inlays and use them as cases. The top part of the cart is removable and can be put inside the cart, that so you can take it everywhere you want. It also has a full lock-down, so nobody can touch your tools.
-        </div>
-          <img src="img/testafbeeldingen/1.jpg" alt="product1"/>
-        <figcaption id="hoverProduct1">
-          <div class="productText">
-            <h3>Toolwelle trolley</h3>
-            <p>Black edition <i class="fa fa-info-circle" aria-hidden="true"></i></p>
-          </div>
-          <div class="price">
-            €1699,-
-          </div>
-        </figcaption>
-      </figure>
-      <figure class="productCard">
-        <div class="productInfoLayer visiblepanel" id="product2">
-          <h2 class="productName">Toolwelle trolley</h2>
-          This is the Black Edition of the tool carts from Toolwelle. It has 2 inlays fully filled with tools. It's possible to remove the inlays and use them as cases. The top part of the cart is removable and can be put inside the cart, that so you can take it everywhere you want. It also has a full lock-down, so nobody can touch your tools.
-        </div>
-        <img src="img/testafbeeldingen/1.jpg" alt="product1"/>
-        <figcaption id="hoverProduct2">
-
-          <div class="productText">
-            <h3>Toolwelle trolley</h3>
-            <p>Black edition <i class="fa fa-info-circle" aria-hidden="true"></i></p>
-          </div>
-          <div class="price">
-            €1699,-
-          </div>
-        </figcaption>
-      </figure>
-      <figure class="productCard">
-        <div class="productInfoLayer visiblepanel" id="product3">
-          <h2 class="productName">Toolwelle trolley</h2>
-              This includes the most comprehensive set of the professional line and stands for ease with this high-quality ring spanners with on one side a ring spanner and a wrench on the other side of the same size. This means you are provided with all standard sizes and this set is a must in every vehicle, home and workshop. The entire set is made ​​of high quality Chrome Vanadium steel and made to last forever and endure large forces. It meets the highest standards and is set with DIN 3113a mark.
-        </div>
-        <img src="img/testafbeeldingen/1.jpg" alt="product1"/>
-        <figcaption id="hoverProduct3">
-
-          <div class="productText">
-            <h3>Toolwelle trolley</h3>
-            <p>Black edition <i class="fa fa-info-circle" aria-hidden="true"></i></p>
-          </div>
-          <div class="price">
-            €1699,-
-          </div>
-        </figcaption>
-      </figure>
-      <figure class="productCard">
-        <div class="productInfoLayer visiblepanel" id="product4">
-          <h2 class="productName">Toolwelle trolley</h2>
-          This is the Black Edition of the tool carts from Toolwelle. It has 2 inlays fully filled with tools. It's possible to remove the inlays and use them as cases. The top part of the cart is removable and can be put inside the cart, that so you can take it everywhere you want. It also has a full lock-down, so nobody can touch your tools.
-        </div>
-        <img src="img/testafbeeldingen/1.jpg" alt="product1"/>
-        <figcaption id="hoverProduct4">
-
-          <div class="productText">
-            <h3>Toolwelle trolley</h3>
-            <p>Black edition <i class="fa fa-info-circle" aria-hidden="true"></i></p>
-          </div>
-          <div class="price">
-            €1699,-
-          </div>
-        </figcaption>
-      </figure>
-      <figure class="productCard">
-        <div class="productInfoLayer visiblepanel" id="product5">
-          <h2 class="productName">Toolwelle trolley</h2>
-          This is the Black Edition of the tool carts from Toolwelle. It has 2 inlays fully filled with tools. It's possible to remove the inlays and use them as cases. The top part of the cart is removable and can be put inside the cart, that so you can take it everywhere you want. It also has a full lock-down, so nobody can touch your tools.
-        </div>
-        <img src="img/testafbeeldingen/1.jpg" alt="product2"/>
-        <figcaption id="hoverProduct5">
-
-          <div class="productText">
-            <h3>Toolwelle trolley</h3>
-            <p>Black edition <i class="fa fa-info-circle" aria-hidden="true"></i></p>
-          </div>
-          <div class="price">
-            €1699,-
-          </div>
-        </figcaption>
-      </figure>
-      <figure class="productCard">
-        <div class="productInfoLayer visiblepanel" id="product6">
-          <h2 class="productName">Toolwelle trolley</h2>
-          This is the Black Edition of the tool carts from Toolwelle. It has 2 inlays fully filled with tools. It's possible to remove the inlays and use them as cases. The top part of the cart is removable and can be put inside the cart, that so you can take it everywhere you want. It also has a full lock-down, so nobody can touch your tools.
-        </div>
-        <img src="img/testafbeeldingen/1.jpg" alt="product3"/>
-        <figcaption id="hoverProduct6">
-
-          <div class="productText">
-            <h3>Toolwelle trolley</h3>
-            <p>Black edition <i class="fa fa-info-circle" aria-hidden="true"></i></p>
-          </div>
-          <div class="price">
-            €1699,-
-          </div>
-        </figcaption>
-      </figure>
+  <?php
+  foreach ($productName as $id => $value) {
+    print("<figure class='productCard'>
+            <div class='productInfoLayer visiblepanel' id='product" . $id . "'>
+              <h2 class='productName'>" . $productName[$id] . "</h2>
+              " . $productDesc[$id] . "
+            </div>
+              <img src='img/products/" . $id . "-small.jpg' alt='product" . $id . "'/>
+            <figcaption id='hoverProduct" . $id . "'>
+              <div class='productText'>
+                <h3>" . $productName[$id] . "</h3>
+                <p>Black edition <i class='fa fa-info-circle' aria-hidden='true'></i></p>
+              </div>
+              <div class='price'>
+                €" . $productPrice[$id] . "
+              </div>
+            </figcaption>
+          </figure>");
+  }
+  ?>
       <div class="invisible">...</div>
   </div>
 </div>
