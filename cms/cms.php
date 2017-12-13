45ac1e2b2c6ca841d3de53d69536c0204de5a1ef
marijnkoel <marijnkoel@live.nl>
2017-12-13 11:14:54 +0100

    fixed my own stupid error

--- cms/cms.php
+++ cms/cms.php
@@ -2,16 +2,14 @@
 include 'include/init.php';
 include 'include/topBar.php';
 include 'include/sideBar.php';
-<<<<<<< HEAD
-?>
 
-	Gesa
+?>
 	</body>
 </html>
 <?php
 	$dbh = null;
 	$stmt = null;
-=======
+
 
 echo "<h1>Wilpe CMS</h1>";
 echo "Klik op een van de dingetjes in het menu links. In het drop down menu boven kan je een website selecteren om te bewerken.";
@@ -20,4 +18,3 @@ echo "</body></html>";
 
 $dbh = null;
 $stmt = null;
->>>>>>> d62e378d8555624814c030a966308a1d79a33e3e
