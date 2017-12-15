96de0893ab1e7ae1fae3603e079cc33d0929428b
barryvdvegt <32839265+barryvdvegt@users.noreply.github.com>
2017-12-15 11:29:51 +0100

    gefixed domain checker

---
+++ cms/check.php
@@ -0,0 +1,20 @@
+<?php
+include 'include/init.php';
+include 'include/topBar.php';
+include 'include/sideBar.php';
+ $domain = $_POST["domein"];
+ if ( gethostbyname($domain) != $domain ) {
+ 	print ("Domein is al bezet");
+ }else{
+ 	print("jahoor die kan");
+ }
+?>
+	<form method="post" action="#">
+		<input type="text" name="domein">
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
+<?php
+	$dbh = null;
+	$stmt = null;
