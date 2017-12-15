d3d3800fe876f418ad4bfaea3a10428ca66e541c
barryvdvegt <32839265+barryvdvegt@users.noreply.github.com>
2017-12-15 15:22:24 +0100

    fixed domain

--- cms/check.php
+++ cms/check.php
@@ -7,22 +7,29 @@ include 'include/arrayDomain.php';
 <h1></h1>
 <br>
 <form method="post" action="#">
-		www.<input type="text" name="domein" placeholder="">.nl/.com enzovoort
+		www.<input type="text" name="domein" placeholder="<?php
+if(empty($_POST["domein"])){ print("voorbeeld: toolwelle");}else{print($_POST["domein"]);}?>">.nl/.com enzovoort
 		<br><input type="submit" value="Submit">
 </form>
+<table>
+	<tr>
+		<th>Domein</th>
+		<th>Beschikbaar</th>
+	</tr>
 <?php
 if(empty($_POST["domein"])){
 }else{
  foreach ($TLD as $key => $value) {
  $domainCheck = ($_POST["domein"] . "." . $value);
+ print ("<tr><td>" . $domainCheck . "</td>");
  if (gethostbyname($domainCheck) != $domainCheck ) {
- 	print ($domainCheck . " is al bezet<br>");
+ 	print ("<td> nee</td></tr>");
  }else{
- 	print($domainCheck . " is bruikbaar<br>");
+ 	print("<td> ja</td></tr>");
  }
 }}
 ?>
-
+</table>
 </body>
 </html>
 <?php
