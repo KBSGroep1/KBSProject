3da8eaa20ab4ddd7bbdd8852bdb33a70c710aefc
barryvdvegt <32839265+barryvdvegt@users.noreply.github.com>
2017-12-22 08:26:49 +0100

    Teije's toevoeging

--- cms/cms.php
+++ cms/cms.php
@@ -3,18 +3,37 @@ include 'include/init.php';
 include 'include/topBar.php';
 include 'include/sideBar.php';
 
-?>
-	</body>
-</html>
-<?php
-	$dbh = null;
-	$stmt = null;
-
-
 echo "<h1>Wilpe CMS</h1>";
-echo "Klik op een van de dingetjes in het menu links. In het drop down menu boven kan je een website selecteren om te bewerken.";
-
-echo "</body></html>";
+if($_SESSION["userRole"] == 3){
+echo "Welkom op de website-editor van Wilpe./n
+Om te beginnen met het editen van uw website selecteert u eerst de betreffende site. Klik hiervoor op de tekst ‘Alle websites’ in de bovenste menubalk./n
+Er zal een dropdown menu verschijnen waarin u kunt kiezen uit alle bestaande websites./n
+Nu bent u klaar om de betreffende site te bewerken./n
+Onder de knop ‘Producten’ kunt nieuwe producten toevoegen en bestaande producten aanpassen./n
+Let op! Heeft u geen website geselecteerd, dan zal u alle producten van alle sites voor u krijgen./n
+Onder ‘Websites’ kunt u een nieuw domein aanmaken. Deze komt dan in de lijst te staan. Selecteer een website om deze te bewerken./n
+In het menu ‘Gebruikers’ kunt u alle gebruikers beheren./n
+Bij ‘Berichten’ komen alle verstuurde contactformulieren binnen.";
+}
+if($_SESSION["userRole"] == 2){
+echo "Om te beginnen met het editen van uw website selecteert u eerst de betreffende site. Klik hiervoor op de tekst ‘Alle websites’ in de bovenste menubalk./n
+Er zal een dropdown menu verschijnen waarin u kunt kiezen uit alle bestaande websites./n
+Nu bent u klaar om de betreffende site te bewerken./n
+Onder de knop ‘Producten’ kunt nieuwe producten toevoegen en bestaande producten aanpassen./n
+Let op! Heeft u geen website geselecteerd, dan zal u alle producten van alle sites voor u krijgen./n
+Onder ‘Websites’ kunt u een nieuw domein aanmaken. Deze komt dan in de lijst te staan. Selecteer een website om deze te bewerken./n
+Bij ‘Berichten’ komen alle verstuurde contactformulieren binnen.";
+}
+if($_SESSION["userRole"] == 1){
+echo "U bent ingelogd als Productbeheerder. Dit betekent dat u alleen toegang heeft om de benodigde producten te uploaden./n    
+Selecteer de juiste website in het dropdown-menu hierboven. Als u geen site selecteert, zult u alle producten te zien krijgen./n
+Vervolgens klikt u op het menu 'Producten' in de linkerbalk. Nu bent u klaar om de producten te beheren./n";    
+}
 
 $dbh = null;
 $stmt = null;
+
+?>
+
+	</body>
+</html>
